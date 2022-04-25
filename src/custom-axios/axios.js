625cce8fb3a1c42b61c8317a421e8582d0099c7a
Marko Andonov <marko.20112000@gmail.com>
2022-04-26 01:22:17 +0200

    Update axios.js

--- src/custom-axios/axios.js
+++ src/custom-axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'https://librarian-worker.herokuapp.com/api',
+    baseURL: 'https://librarian-worker-backend.herokuapp.com/api',
     headers: {
         'Access-Control-Allow-Origin' : '*'
     }
